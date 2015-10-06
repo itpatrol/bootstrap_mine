e567320455db0b993861b35ac469f97528de2c87
Gor Martsen <Gor.martsen@gmail.com>
2015-10-05 23:24:53 -0400

    Properly implement navigation bootstrap

---
+++ header.tpl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @file
+ * Display generic site information such as logo, site name, etc.
+ *
+ * Available variables:
+ *
+ * - $base_path: The base URL path of the Backdrop installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $front_page: The URL of the front page. Use this instead of $base_path, when
+ *   linking to the front page. This includes the language domain or prefix.
+ * - $site_name: The name of the site, empty when display has been disabled.
+ * - $site_slogan: The site slogan, empty when display has been disabled.
+ * - $menu: The menu for the header (if any), as an HTML string.
+ */
+?>
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="container">
+    <div class="navbar-header">
+    <?php if ($logo): ?>
+      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+    <?php if (!empty($site_name)): ?>
+      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+    <?php endif; ?>
+    
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    
+    <?php if ($menu): ?>
+      <div class="navbar-collapse collapse">
+        <nav role="navigation">
+        <?php print $menu; ?>
+        </nav>
+      </div>
+    <?php endif; ?>
+  </div>
+</header>
