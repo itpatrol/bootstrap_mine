a4f4ddde69f232916c1716ef6d8f0e149c7ee977
Gor Martsen <Gor.martsen@gmail.com>
2015-10-06 21:03:14 -0400

    remove debug code

--- template.php
+++ template.php
@@ -78,7 +78,6 @@ function bootstrap_menu_tree($variables){
 }
 
 function bootstrap_links__header_menu($menu){
-  print_r($menu);
   $menu['attributes']['class'] = array('menu','nav','navbar-nav');
   return theme_links($menu);
 }
