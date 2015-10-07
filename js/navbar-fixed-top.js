91af645e95ecf0543b79882eecf1e23beef4892b
Gor Martsen <Gor.martsen@gmail.com>
2015-10-07 11:24:55 -0400

    Add throttle here

--- js/navbar-fixed-top.js
+++ js/navbar-fixed-top.js
@@ -8,14 +8,14 @@
     // run Javascript on page load here
     console.log("Welcome to the console");
 
-    jQuery(window).scroll(function() {
+    jQuery(window).scroll($.throttle( 100, function() {
       var win = jQuery(this);
       if (win.scrollTop() > 33) {
         $("html.admin-bar header.navbar-fixed-top").css("top", "0px");
       } else {
         $("html.admin-bar header.navbar-fixed-top").css("top",  (33 - win.scrollTop()) + "px");
       }
-    });
+    }));
 
 
   });
