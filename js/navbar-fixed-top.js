fe0a37c25f9d039c0b3eed46cd992e539503d9da
Gor Martsen <Gor.martsen@gmail.com>
2015-10-07 10:49:41 -0400

    Use class here

--- js/navbar-fixed-top.js
+++ js/navbar-fixed-top.js
@@ -3,7 +3,7 @@
  * Attach administrative behaviors.
  */
 Backdrop.bootstrap.attachBehaviors = function (context, settings) {
-  $( "body" ).scroll(function() {
+  $("body.navbar-is-fixed-top" ).scroll(function() {
     alert('test');
   });
 };
