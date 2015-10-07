2416f9676651050a8b95e14b74fc73df4788199e
Gor Martsen <Gor.martsen@gmail.com>
2015-10-06 22:13:17 -0400

    Debug code

--- template.php
+++ template.php
@@ -82,7 +82,9 @@ function bootstrap_preprocess_header(&$variables){
   if($navbar_position = theme_get_setting('bootstrap_navbar_user_menu'))
   {
     $user_menu = menu_tree('user-menu');
+    print_r($user_menu);
     $variables['navigation'] = render($user_menu);
+    print_r($variables['navigation']);
   }
   
   $variables['navbar_classes_array'] = array('navbar');
