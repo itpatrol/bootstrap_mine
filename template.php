d1c862b1832cf91ece24fab79b70d0803812e287
Gor Martsen <Gor.martsen@gmail.com>
2015-10-07 22:21:44 -0400

    clean template.php, replace drupal with backdrop

--- template.php
+++ template.php
@@ -2,26 +2,6 @@
 /**
  * @file
  * template.php
- *
- * This file should only contain light helper functions and stubs pointing to
- * other files containing more complex functions.
- *
- * The stubs should point to files within the `theme` folder named after the
- * function itself minus the theme prefix. If the stub contains a group of
- * functions, then please organize them so they are related in some way and name
- * the file appropriately to at least hint at what it contains.
- *
- * All [pre]process functions, theme functions and template implementations also
- * live in the 'theme' folder. This is a highly automated and complex system
- * designed to only load the necessary files when a given theme hook is invoked.
- * @see _bootstrap_theme()
- * @see theme/registry.inc
- *
- * Due to a bug in Drush, these includes must live inside the 'theme' folder
- * instead of something like 'includes'. If a module or theme has an 'includes'
- * folder, Drush will think it is trying to bootstrap core when it is invoked
- * from inside the particular extension's directory.
- * @see https://drupal.org/node/2102287
  */
 
 /**
@@ -104,7 +84,7 @@ function bootstrap_css_alter(&$css) {
 function bootstrap_js_alter(&$js) {
   if (theme_get_setting('bootstrap_cdn')) {
     $cdn = '//netdna.bootstrapcdn.com/bootstrap/' . theme_get_setting('bootstrap_cdn')  . '/js/bootstrap.min.js';
-    $js[$cdn] = drupal_js_defaults();
+    $js[$cdn] = backdrop_js_defaults();
     $js[$cdn]['data'] = $cdn;
     $js[$cdn]['type'] = 'external';
     $js[$cdn]['every_page'] = TRUE;
