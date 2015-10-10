d1e5e85c349dc09d1022db8c2f1ca7a71befb8cc
Gor Martsen <Gor.martsen@gmail.com>
2015-10-09 21:54:46 -0400

    Implements bootstrap_system_admin_index

--- template.php
+++ template.php
@@ -460,6 +460,57 @@ function bootstrap_admin_block($variables) {
 }
 
 /**
+ * Returns HTML for the output of the dashboard page.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - menu_items: An array of modules to be displayed.
+ *
+ * @ingroup themeable
+ */
+function bootstrap_system_admin_index($variables) {
+  $menu_items = $variables['menu_items'];
+
+  $stripe = 0;
+  $container = array('left' => '', 'right' => '');
+  $flip = array('left' => 'right', 'right' => 'left');
+  $position = 'left';
+
+  // Iterate over all modules.
+  foreach ($menu_items as $module => $block) {
+    list($description, $items) = $block;
+
+    // Output links.
+    if (count($items)) {
+      $block = array();
+      $block['title'] = $module;
+      $block['content'] = theme('admin_block_content', array('content' => $items));
+      $block['description'] = t($description);
+      $block['show'] = TRUE;
+
+      if ($block_output = theme('admin_block', array('block' => $block))) {
+        if (!isset($block['position'])) {
+          // Perform automatic striping.
+          $block['position'] = $position;
+          $position = $flip[$position];
+        }
+        $container[$block['position']] .= $block_output;
+      }
+    }
+  }
+
+  $output = '<div class="admin clearfix">';
+  foreach ($container as $id => $data) {
+    $output .= '<div class="' . $id . ' col-md-6 col-sm-12 clearfix">';
+    $output .= $data;
+    $output .= '</div>';
+  }
+  $output .= '</div>';
+
+  return $output;
+}
+
+/**
  * Returns HTML for an administrative page.
  *
  * @param $variables
@@ -475,15 +526,28 @@ function bootstrap_admin_page($variables) {
   $blocks = $variables['blocks'];
 
   $stripe = 0;
+  $container = array();
 
-  $output = '<div class="admin clearfix">';
-  
   foreach ($blocks as $block) {
-    $output .= '<div class="col-md-6 col-sm-12">';
-    $output .= theme('admin_block', array('block' => $block));
-    $output .= '</div>';
+    if ($block_output = theme('admin_block', array('block' => $block))) {
+      if (empty($block['position'])) {
+        // perform automatic striping.
+        $block['position'] = ++$stripe % 2 ? 'left' : 'right';
+      }
+      if (!isset($container[$block['position']])) {
+        $container[$block['position']] = '';
+      }
+      $container[$block['position']] .= $block_output;
+    }
   }
 
+  $output = '<div class="admin clearfix">';
+
+  foreach ($container as $id => $data) {
+    $output .= '<div class="' . $id . ' clearfix  col-md-6 col-sm-12 ">';
+    $output .= $data;
+    $output .= '</div>';
+  }
   $output .= '</div>';
   return $output;
 }
