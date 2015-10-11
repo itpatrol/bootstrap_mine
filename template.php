b644b7e7ea9736fe13e8f4ccf39c6cfa07c12765
Gor Martsen <Gor.martsen@gmail.com>
2015-10-11 12:29:05 -0400

    Use bootstrap style breadcrups

--- template.php
+++ template.php
@@ -637,3 +637,23 @@ function bootstrap_links__dropbutton($menu){
   return theme_links($menu);
 }
 
+/**
+ * Returns HTML for a breadcrumb trail.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - breadcrumb: An array containing the breadcrumb links.
+ */
+function bootstrap_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $output = '';
+  if (!empty($breadcrumb)) {
+    $output .= '<nav role="navigation">';
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output .= '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $output .= '<ol  class="breadcrumb" ><li>' . implode('</li><li>', $breadcrumb) . '</li></ol>';
+    $output .= '</nav>';
+  }
+  return $output;
+}
