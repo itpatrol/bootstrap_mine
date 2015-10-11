bdb90059248577e2495de796b6ab88ad1f8898df
Gor Martsen <Gor.martsen@gmail.com>
2015-10-11 09:14:52 -0400

    Add form control to select

--- template.php
+++ template.php
@@ -406,11 +406,29 @@ function bootstrap_password($variables) {
  * @ingroup themeable
  */
 function bootstrap_search($variables) {
-  $element = $variables['element'];
   $variables['element']['#attributes']['class'][] = 'form-control';
   return theme_search($variables);
 }
 
+/**
+ * Returns HTML for a select form element.
+ *
+ * It is possible to group options together; to do this, change the format of
+ * $options to an associative array in which the keys are group labels, and the
+ * values are associative arrays in the normal $options format.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - element: An associative array containing the properties of the element.
+ *     Properties used: #title, #value, #options, #description, #extra,
+ *     #multiple, #required, #name, #attributes, #size.
+ *
+ * @ingroup themeable
+ */
+function bootstrap_select($variables) {
+  $variables['element']['#attributes']['class'][] = 'form-control';
+  return theme_select($variables);
+}
 
 /**
  * Implements hook_preprocess_table().
