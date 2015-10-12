272d66aa76318997c0c3bdca2a4f2e360080e0cd
Gor Martsen <Gor.martsen@gmail.com>
2015-10-11 22:10:35 -0400

    If there is no header, do not show panel-heading

--- templates/views-ui-display-tab-bucket.tpl.php
+++ templates/views-ui-display-tab-bucket.tpl.php
@@ -5,7 +5,9 @@
  */
 ?>
 <div class="<?php print implode(' ', $classes); ?> panel panel-default"<?php print backdrop_attributes($attributes); ?>>
+  <?php if (!empty($title) && !empty($actions)) : ?>
   <div class="panel-heading">
+  <?php endif; ?>  
   <?php if (!empty($title)) : ?>
     <h3 class="panel-title"><?php print $title; ?></h3>
   <?php endif; ?>
@@ -13,7 +15,9 @@
   <?php if(!empty($actions)) : ?>
     <?php print $actions; ?>
   <?php endif; ?>
+  <?php if (!empty($title) && !empty($actions)) : ?>
   </div>
+  <?php endif; ?>
   <div class="panel-body">
   <?php print $content; ?>
   </div>
