07f0d3f1c63ee3a32b92d644cfd6666d2d4ed8c8
Gor Martsen <Gor.martsen@gmail.com>
2015-10-12 14:44:24 -0400

    Implements bootstrap_preprocess_user_picture

--- template.php
+++ template.php
@@ -814,3 +814,51 @@ function bootstrap_admin_block_content($variables) {
   return bootstrap_node_add_list($variables);
 }
 
+/**
+ * Process variables for user-picture.tpl.php.
+ *
+ * The $variables array contains the following arguments:
+ * - $account: A user, node or comment object with 'name', 'uid' and 'picture'
+ *   fields.
+ *
+ * @see user-picture.tpl.php
+ */
+function bootstrap_preprocess_user_picture(&$variables) {
+  $variables['user_picture'] = '';
+  if (config_get('system.core', 'user_pictures')) {
+    $account = $variables['account'];
+    if (!empty($account->picture)) {
+      // @TODO: Ideally this function would only be passed file entities, but
+      // since there's a lot of legacy code that JOINs the {users} table to
+      // {node} or {comments} and passes the results into this function if we
+      // a numeric value in the picture field we'll assume it's a file id
+      // and load it for them. Once we've got user_load_multiple() and
+      // comment_load_multiple() functions the user module will be able to load
+      // the picture files in mass during the object's load process.
+      if (is_numeric($account->picture)) {
+        $account->picture = file_load($account->picture);
+      }
+      if (!empty($account->picture->uri)) {
+        $filepath = $account->picture->uri;
+      }
+    }
+    elseif (config_get('system.core', 'user_picture_default')) {
+      $filepath = config_get('system.core', 'user_picture_default');
+    }
+    if (isset($filepath)) {
+      $alt = t("@user's picture", array('@user' => user_format_name($account)));
+      // If the image does not have a valid Backdrop scheme (for eg. HTTP),
+      // don't load image styles.
+      if (module_exists('image') && file_valid_uri($filepath) && $style = config_get('system.core', 'user_picture_style')) {
+        $variables['user_picture'] = theme('image_style', array('style_name' => $style, 'uri' => $filepath, 'alt' => $alt, 'title' => $alt, 'attributes' => array('class' => 'img-circle')));
+      }
+      else {
+        $variables['user_picture'] = theme('image', array('uri' => $filepath, 'alt' => $alt, 'title' => $alt, 'attributes' => array('class' => 'img-circle')));
+      }
+      if (!empty($account->uid) && user_access('access user profiles')) {
+        $attributes = array('attributes' => array('title' => t('View user profile.')), 'html' => TRUE);
+        $variables['user_picture'] = l($variables['user_picture'], "user/$account->uid", $attributes);
+      }
+    }
+  }
+}
