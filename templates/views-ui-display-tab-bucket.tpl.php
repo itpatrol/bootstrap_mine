070295caf905ad95b0dc3d826b9cfdbac0d406c1
Gor Martsen <Gor.martsen@gmail.com>
2015-10-11 22:00:12 -0400

    rearrange style

--- templates/views-ui-display-tab-bucket.tpl.php
+++ templates/views-ui-display-tab-bucket.tpl.php
@@ -5,15 +5,15 @@
  */
 ?>
 <div class="<?php print implode(' ', $classes); ?> panel panel-default"<?php print backdrop_attributes($attributes); ?>>
+  <div class="panel-heading">
+  <?php if (!empty($title)) : ?>
+    <h3 class="panel-title"><?php print $title; ?></h3>
+  <?php endif; ?>
   <?php print $item_help_icon; ?>
   <?php if(!empty($actions)) : ?>
     <?php print $actions; ?>
   <?php endif; ?>
-  <?php if (!empty($title)) : ?>
-    <div class="panel-heading">
-    <h3 class="panel-title"><?php print $title; ?></h3>
-    </div>
-  <?php endif; ?>
+  </div>
   <div class="panel-body">
   <?php print $content; ?>
   </div>
