7a899bced6753e1585ece8b3980fdb6444ead2ed
Gor Martsen <Gor.martsen@gmail.com>
2015-10-13 11:28:22 -0400

    remove debug code

--- template.php
+++ template.php
@@ -122,7 +122,6 @@ function bootstrap_preprocess_layout(&$variables) {
 
 function bootstrap_preprocess_page(&$variables){
 
-  print_r($variables);
   $no_old_ie_compatibility_modes = array(
     '#tag' => 'meta',
     '#attributes' => array(
