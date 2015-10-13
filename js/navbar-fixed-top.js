ef658059f5b156d481f78ebbac49cd7b596ae6d8
Gor Martsen <Gor.martsen@gmail.com>
2015-10-13 10:43:40 -0400

    Roll back script

--- js/navbar-fixed-top.js
+++ js/navbar-fixed-top.js
@@ -5,12 +5,19 @@
 (function($, Backdrop, window, document, undefined) {
   $(document).ready(function() {
     $("#navbar").detach().prependTo('body');
+    $("#navbar").removeClass('navbar-fixed-top');
+    $("body").removeClass('navbar-is-fixed-top');
+    $("#navbar").addClass('navbar-static-top');
 
     jQuery(window).scroll(function() {
       var win = jQuery(this);
       if (win.scrollTop() > 33) {
+        $("#navbar").addClass('navbar-fixed-top');
+        $("#navbar").removeClass('navbar-static-top');
         $("body").addClass('navbar-is-fixed-top-padding');
       } else {
+        $("#navbar").removeClass('navbar-fixed-top');
+        $("#navbar").addClass('navbar-static-top');
         $("body").removeClass('navbar-is-fixed-top-padding');
       }
     });
