a517cbca2c553dcfbc1c6c3cf585948e3acf8cbd
Gor Martsen <Gor.martsen@gmail.com>
2015-10-12 20:36:15 -0400

    Make sure we have $form['actions']['cancel']['#type']

--- template.php
+++ template.php
@@ -766,7 +766,7 @@ function bootstrap_preprocess_views_view_table(&$variables) {
 function bootstrap_form_alter(array &$form, array &$form_state = array(), $form_id = NULL) {
   if ($form_id) {
 //    print_r($form);
-    if(isset($form['actions']['cancel']) && $form['actions']['cancel']['#type'] == 'link'){
+    if(isset($form['actions']['cancel']) && isset($form['actions']['cancel']['#type']) && $form['actions']['cancel']['#type'] == 'link'){
        $form['actions']['cancel']['#options']['attributes']['class'][] = 'btn';
        $form['actions']['cancel']['#options']['attributes']['class'][] = 'btn-default';
     }
