86f6765a8508e80a7b7356c6adcaa34fb3ad3f6e
Gor Martsen <Gor.martsen@gmail.com>
2015-10-13 11:22:44 -0400

    Make sure we do have navbar

--- js/navbar-fixed-top.js
+++ js/navbar-fixed-top.js
@@ -4,25 +4,25 @@
  */
 (function($, Backdrop, window, document, undefined) {
   $(document).ready(function() {
-    $("#navbar").detach().prependTo('body');
-    $("#navbar").removeClass('navbar-fixed-top');
-    $("body").removeClass('navbar-is-fixed-top');
-    $("#navbar").addClass('navbar-static-top');
-    $("#navbar").addClass('navbar-static-top-padding');
-
-    jQuery(window).scroll(function() {
-      var win = jQuery(this);
-      if (win.scrollTop() > 33) {
-        $("#navbar").addClass('navbar-fixed-top');
-        $("#navbar").removeClass('navbar-static-top');
-        $("body").addClass('navbar-is-fixed-top-padding');
-      } else {
-        $("#navbar").removeClass('navbar-fixed-top');
-        $("#navbar").addClass('navbar-static-top');
-        $("body").removeClass('navbar-is-fixed-top-padding');
-      }
-    });
-
-
+    if ($("#navbar").length){
+      $("#navbar").detach().prependTo('body');
+      $("#navbar").removeClass('navbar-fixed-top');
+      $("body").removeClass('navbar-is-fixed-top');
+      $("#navbar").addClass('navbar-static-top');
+      $("#navbar").addClass('navbar-static-top-padding');
+  
+      jQuery(window).scroll(function() {
+        var win = jQuery(this);
+        if (win.scrollTop() > 33) {
+          $("#navbar").addClass('navbar-fixed-top');
+          $("#navbar").removeClass('navbar-static-top');
+          $("body").addClass('navbar-is-fixed-top-padding');
+        } else {
+          $("#navbar").removeClass('navbar-fixed-top');
+          $("#navbar").addClass('navbar-static-top');
+          $("body").removeClass('navbar-is-fixed-top-padding');
+        }
+      });
+    }
   });
 })(jQuery, Backdrop, this, this.document);
